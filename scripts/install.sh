cf2c7cf8162b597f32eeb68ca02fd198f394dc14
shlee9412 <rlsh7321@gmail.com>
2023-02-06 14:26:00 +0900

    install 스크립트 수정

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,6 +6,8 @@ if [ $(docker-compose ps -a | grep $COMPOSE_PROJECT_NAME-frontend | wc -l) -gt 0
   $DOCKER_COMPOSE stop frontend
   $DOCKER_COMPOSE rm -f frontend
 fi
+if [ $(docker images | grep $COMPOSE_PROJECT_NAME-frontend | grep $FRONTEND_VERSION | wc -l) -gt 0 ]; then
+  docker rmi $COMPOSE_PROJECT_NAME-frontend:$FRONTEND_VERSION # stockbox-nginx:$FRONTEND_VERSION
+fi
 docker system prune -f
-docker rmi stockbox-frontend:$FRONTEND_VERSION # stockbox-nginx:$FRONTEND_VERSION
 $DOCKER_COMPOSE up -d frontend
