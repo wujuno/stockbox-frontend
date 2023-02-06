63010626d869218391d1d324e95e8aaae063b490
shlee9412 <rlsh7321@gmail.com>
2023-02-06 20:46:22 +0900

    docker compose 수정

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,7 +7,7 @@ if [ $(docker-compose ps -a | grep $COMPOSE_PROJECT_NAME-frontend | wc -l) -gt 0
   $DOCKER_COMPOSE rm -f frontend
 fi
 if [ $(docker images | grep $COMPOSE_PROJECT_NAME-frontend | grep $FRONTEND_VERSION | wc -l) -gt 0 ]; then
-  docker rmi $COMPOSE_PROJECT_NAME-frontend:$FRONTEND_VERSION # stockbox-nginx:$FRONTEND_VERSION
+  docker rmi $COMPOSE_PROJECT_NAME-frontend:$FRONTEND_VERSION stockbox-nginx:$FRONTEND_VERSION
 fi
 docker system prune -f
 $DOCKER_COMPOSE up -d frontend
