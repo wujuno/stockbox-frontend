f5355738fcaf0a14353e77cb2d9256d9a7192086
rlsh7321 <rlsh7321@gmail.com>
2023-02-08 00:06:51 +0900

    eslintrc 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,11 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: ['@remix-run/eslint-config', '@remix-run/eslint-config/node', 'prettier/@typescript-eslint', 'prettier'],
+  extends: ['@remix-run/eslint-config', '@remix-run/eslint-config/node', 'prettier'],
   plugins: ['prettier'],
   rules: {
-    '@typescript-eslint/consistent-type-imports': 'off'
+    '@typescript-eslint/consistent-type-imports': 'off',
+    'prettier/prettier': 'error',
+    'arrow-body-style': 'off',
+    'prefer-arrow-callback': 'off'
   }
 };
