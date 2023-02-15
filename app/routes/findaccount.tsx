3734f39656462d0cdfa94ec641179afb831ee4c8
shlee9412 <rlsh7321@gmail.com>
2023-02-15 19:48:22 +0900

    계정찾기 페이지

--- app/routes/findaccount.tsx
+++ app/routes/findaccount.tsx
@@ -1,7 +1,43 @@
+import { useState } from 'react';
+import { Box, Tab, Tabs } from '@mui/material';
+import styled from '@emotion/styled';
+import { useTranslation } from 'react-i18next';
+import { DataFunctionArgs, json } from '@remix-run/node';
 import { Page } from '@/components/Layout';
+import { loaderCommonInit } from '@/lib/loaderCommon';
+
+export const loader = async ({ request }: DataFunctionArgs) => {
+  try {
+    const result = await loaderCommonInit(request);
+    if (result) return result;
+  } catch (err) {
+    console.error(err);
+  } finally {
+    return json(null);
+  }
+};
+
+export const handle = {
+  i18n: 'findAccount'
+};
+
+const Container = styled(Page)``;
 
 const FindAccount = () => {
-  return <Page></Page>;
+  const [tabNum, setTabNum] = useState(0);
+
+  const { t } = useTranslation('findAccount');
+
+  return (
+    <Container>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={tabNum} onChange={(e, newTabNum) => setTabNum(newTabNum)} aria-label="find account tabs">
+          <Tab label={t('findEmail')} />
+          <Tab label={t('findPassword')} />
+        </Tabs>
+      </Box>
+    </Container>
+  );
 };
 
 export default FindAccount;
