4bff333c2bfb92431c33dd57b9d1c4efe6823b85
wujuno <huj88854@gmail.com>
2023-03-30 19:47:43 +0900

    Feat: 루트 페이지 히트맵 구현

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,14 +1,15 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useState } from 'react';
 import { Page } from '@/components/Layout';
-import { Skeleton, Typography } from '@mui/material';
+import { Divider, Skeleton, Typography } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { useHydrated } from 'remix-utils';
 import { DataFunctionArgs, json } from '@remix-run/node';
 import { loaderCommonInit } from '@/lib/loaderCommon';
 import { useRecoilValue } from 'recoil';
 import { stockChartState } from '@/atoms';
+import axios from 'axios';
 
-const SChart = React.lazy(() => import('@/components/Chart'));
+const TreemapChart = React.lazy(() => import('@/components/chart/TreeMapChart'));
 
 /* export const loader = async ({ request }: DataFunctionArgs) => {
   try {
@@ -22,14 +23,30 @@ const SChart = React.lazy(() => import('@/components/Chart'));
 
 const Index = () => {
   const data = useRecoilValue(stockChartState);
+  const [usData, setUsData] = useState();
+  const [kData, setKData] = useState();
 
   const isHydrated = useHydrated();
   const { t } = useTranslation();
 
+  axios.get('/api/pairtrading/heatmap/?country=US&limit=50').then(response => {
+    setUsData(JSON.parse(response));
+  });
+  axios.get('/api/pairtrading/heatmap/?country=KOR&limit=50').then(response => {
+    setKData(JSON.parse(response));
+  });
+
   return (
     <Page>
-      <Typography variant="h3">{t('hello')}</Typography>
-      <Suspense>{isHydrated ? <SChart data={data} /> : <Skeleton variant="rounded" animation="wave" width={500} height={500} />}</Suspense>
+      <Typography variant="h3">미국 주식</Typography>
+      <Suspense>
+        {isHydrated ? <TreemapChart data={usData} width={1200} height={500} title={'해외 주식'} dark={false} /> : <Skeleton variant="rounded" animation="wave" width={1200} height={500} />}
+      </Suspense>
+      <Divider />
+      <Typography variant="h3">국내 주식</Typography>
+      <Suspense>
+        {isHydrated ? <TreemapChart data={kData} width={1200} colors={500} title={'국내 주식'} dark={false} /> : <Skeleton variant="rounded" animation="wave" width={1200} height={500} />}
+      </Suspense>
     </Page>
   );
 };
