034a26072ddc0e69ebc4a4326a7d6153c1345ebd
wujuno <huj88854@gmail.com>
2023-04-20 00:11:43 +0900

    Feat: page pagination 구현

--- app/components/board/BoardList.tsx
+++ app/components/board/BoardList.tsx
@@ -1,11 +1,20 @@
 import { postListState } from '@/atoms';
-import { Box, Button, Pagination, Paper, Stack, Table, TableBody, TableCell, TableContainer, TableFooter, TableHead, TablePagination, TableRow } from '@mui/material';
-import { useState } from 'react';
+import { Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
 import { useRecoilValue } from 'recoil';
 
-const BoardList = () => {
+interface IBoardListProps {
+  currPage: number;
+  pageDivNum: number;
+}
+
+const BoardList: React.FC<IBoardListProps> = ({ currPage, pageDivNum }) => {
   //읽기 전용 값 깊은 복사
-  const postData = [...useRecoilValue(postListState)];
+  const postData = [...useRecoilValue(postListState)].sort((a, b) => b.id - a.id);
+  const dividedData = [];
+  for (let i = 0; i < postData.length; i += pageDivNum) {
+    dividedData.push(postData.slice(i, i + pageDivNum));
+  }
+  let viewData = dividedData[currPage - 1];
 
   return (
     <>
@@ -13,7 +22,7 @@ const BoardList = () => {
         <Table>
           <TableHead>
             <TableRow>
-              <TableCell>id</TableCell>
+              <TableCell>No.</TableCell>
               <TableCell sx={{ minWidth: 250 }} align="center">
                 title
               </TableCell>
@@ -27,23 +36,21 @@ const BoardList = () => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {postData
-              .sort((a, b) => b.id - a.id)
-              .map(post => (
-                <TableRow key={post.id} user_id={post.user.id}>
-                  <TableCell>{post.id}</TableCell>
-                  <TableCell sx={{ maxWidth: 300, textOverflow: 'ellipsis', overflow: 'hidden', whiteSpace: 'nowrap' }} align="center">
-                    {post.title}
-                  </TableCell>
-                  <TableCell sx={{ maxWidth: 100, textOverflow: 'ellipsis', overflow: 'hidden', whiteSpace: 'nowrap' }} align="right">
-                    {post.user.nickname}
-                  </TableCell>
-                  <TableCell align="right">{post.date_updated.slice(0, 10)}</TableCell>
-                  <TableCell sx={{ maxWidth: 50 }}>
-                    <Button> ﹕</Button>
-                  </TableCell>
-                </TableRow>
-              ))}
+            {viewData?.map(post => (
+              <TableRow key={post.id} user_id={post.user.id}>
+                <TableCell>{post.id}</TableCell>
+                <TableCell sx={{ maxWidth: 300, textOverflow: 'ellipsis', overflow: 'hidden', whiteSpace: 'nowrap' }} align="center">
+                  {post.title}
+                </TableCell>
+                <TableCell sx={{ maxWidth: 100, textOverflow: 'ellipsis', overflow: 'hidden', whiteSpace: 'nowrap' }} align="right">
+                  {post.user.nickname}
+                </TableCell>
+                <TableCell align="right">{post.date_updated.slice(0, 10)}</TableCell>
+                <TableCell sx={{ maxWidth: 50 }}>
+                  <Button> ﹕</Button>
+                </TableCell>
+              </TableRow>
+            ))}
           </TableBody>
         </Table>
       </TableContainer>
--- app/routes/board/index.tsx
+++ app/routes/board/index.tsx
@@ -6,7 +6,7 @@ import { Page } from '@/components/Layout';
 import BoardList from '@/components/board/BoardList';
 import { Box, Button, Container, Pagination, Stack, Typography } from '@mui/material';
 import axios from 'axios';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useRecoilState } from 'recoil';
 import { useNavigate } from '@remix-run/react';
 
@@ -24,6 +24,9 @@ export const loader = async ({ request }: DataFunctionArgs) => {
 
 const PostMain = () => {
   const [postData, setPostData] = useRecoilState(postListState);
+  const [currPage, setCurrPage] = useState(1);
+  //보여지는 list 개수
+  const PAGEDIVNUM = 3;
 
   const navigate = useNavigate();
 
@@ -43,14 +46,13 @@ const PostMain = () => {
         <Typography sx={{ mb: 5 }} variant="h3">
           Board
         </Typography>
-
-        <BoardList />
+        <BoardList currPage={currPage} pageDivNum={PAGEDIVNUM} />
         <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', mt: 1 }}>
           <Button variant="contained" sx={{ mb: 1 }} onClick={() => navigate('userId/newpost')}>
             게시물 등록
           </Button>
           <Stack spacing={2}>
-            <Pagination count={Math.ceil(postData.length / 1)} color="primary" />
+            <Pagination count={Math.ceil(postData.length / PAGEDIVNUM)} color="primary" onChange={(e, value) => setCurrPage(value)} />
           </Stack>
         </Box>
       </Container>
